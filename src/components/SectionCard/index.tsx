3214c9d75c696bf4620935d7f06acf556862108b
doomling <33739951+doomling@users.noreply.github.com>
2022-02-18 17:55:56 -0300

    asset prefix not appending to statics

--- src/components/SectionCard/index.tsx
+++ src/components/SectionCard/index.tsx
@@ -14,7 +14,7 @@ export default function SectionCard({
   return (
     <div className="section-card" style={{ background: bgColor }}>
       <Link to={url}>
-        <img src={`${img}`} />
+        <img src={`/${img}`} />
         <div className="section-card-info">
           <h3>{title}</h3>
           <p>{description}</p>
