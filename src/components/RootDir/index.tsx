d48e6138679b8cc6caa0b4c5b40853b92f7b8b21
doomling <33739951+doomling@users.noreply.github.com>
2022-03-18 19:26:53 -0300

    filesystem data from graphql

--- src/components/RootDir/index.tsx
+++ src/components/RootDir/index.tsx
@@ -5,37 +5,36 @@ import "./style.scss";
 
 type Props = {
   name: string;
-  icon: string;
+  offset: number;
   children?: JSX.Element[]; // TODO - verify type
 };
 
 export default function RootDir(props: Props) {
-  const { name, icon, children } = props;
+  const { name, children, offset } = props;
   const [open, setOpen] = useState<boolean>(false);
 
   console.log(open, 123)
 
   return (
     <>
-      <div className="root-container" onClick={() => setOpen(prevState => !prevState )}>
-        <div className="root-title">
+      <div className="root-container">
+        <div className={children && "root-title"} onClick={() => setOpen(prevState => !prevState )} style={{ paddingLeft: `${10 * offset}px` }}>
           {/* <img
             className="icon"
             src={`https://cdn.decentraland.org${withPrefix(`/${name}.svg`)}`}
           /> */}
           {name}
         </div>
-        <div className={open ? "child-container" : "child-container-collapsed"}          
->
+        <div className={open ? "child-container" : "child-container-collapsed"} style={{ paddingLeft: `${10 * offset}px` }}>
         {children &&
           // TO-DO: type the objects, need to define data structure first
           children.map((item: any, key: number) => {
             return (
-              <ChildDir
+              <RootDir
                 name={item.name}
                 children={item.children}
                 offset={1}
-                type={item.type}
+                // type={item.type}
                 key={key}
               />
             );
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,20 +1,49 @@
 import React from 'react'
+import { graphql, StaticQuery, useStaticQuery } from 'gatsby'
 import RootDir from '../RootDir'
 import './style.css'
 
 import menu from '../../mocks/menu.json'
+import { CommentContent } from 'decentraland-ui'
 
 export default function Sidebar() {
+
+  const data = useStaticQuery(graphql`
+  {
+    allFile(filter: {sourceInstanceName: {eq: "pages"}}) {
+      edges {
+        node {
+          relativePath
+          name
+          relativeDirectory
+          root
+          extension
+        }
+      }
+    }
+  }
+
+`)
+
+function generateMenu(data) {
+  data.map(item => {
+    console.log(item)
+  })
+}
+
+generateMenu(data.allFile.edges)
+
   return (
     <aside className="sidebar-container">
       <span>This is a sidebar</span>
+      
 
       {menu.data &&
         menu.data.map((item: any, key: number) => {
           return (
             <RootDir
               name={item.name}
-              icon={item.icon}
+              offset={0}
               children={item.children}
               key={key}
             />
