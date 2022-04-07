4e80f2d9d8556f7f2682251866e3d3048d3f867d
doomling <33739951+doomling@users.noreply.github.com>
2022-04-07 15:42:20 -0300

    welcome image paths

--- src/components/SectionCard/index.tsx
+++ src/components/SectionCard/index.tsx
@@ -3,6 +3,7 @@ import './types'
 import './style.scss'
 import { Link } from 'gatsby'
 import { SectionCardProps } from './types'
+import formatPaths from '@utils/formatPaths'
 
 export default function SectionCard({
   img,
@@ -14,7 +15,7 @@ export default function SectionCard({
   return (
     <div className="section-card" style={{ background: bgColor }}>
       <Link to={url}>
-        <img src={img} />
+        <img src={formatPaths(img)} />
         <div className="section-card-info">
           <h3>{title}</h3>
           <p>{description}</p>
