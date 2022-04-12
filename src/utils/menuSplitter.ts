bd15425dfdb7eb8e1da36d279728206b3f50cb5a
doomling <33739951+doomling@users.noreply.github.com>
2022-04-12 19:45:38 -0300

    partial menu generator

--- src/utils/menuSplitter.ts
+++ src/utils/menuSplitter.ts
@@ -2,20 +2,21 @@ import * as fs from 'fs';
 
 export default function menuSplitter() {
 
-  const menu = {
-    contributor: [],
-    user: [],
-    creator: []
-  }
+  const menu = {}
 
-  fs.readdir("./src/menu-data/", (err, files) => {
+  fs.readdir("./src/menu-data/", async (err, files) => {
     if (err)
       console.log(err);
     else {
       files.forEach(file => {
         try {
-          const data = fs.readFileSync("./src/menu-data/" + file, 'utf8')
-          console.log(data)
+          const data = JSON.parse(fs.readFileSync("./src/menu-data/" + file, 'utf8'))
+
+          Object.keys(data).map((key) => {
+            menu[key] = menu[key] ? [...menu[key], data[key]] : [data[key]]
+          })
+
+          fs.writeFileSync("menu.json", JSON.stringify(menu))
         } catch (err) {
           console.error(err)
         }
