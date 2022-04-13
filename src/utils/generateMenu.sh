c847f8daeb89faee6a9d59ce99dec8c4ccb1a5cd
doomling <33739951+doomling@users.noreply.github.com>
2022-04-13 16:05:18 -0300

    fix menu generation

--- src/utils/generateMenu.sh
+++ src/utils/generateMenu.sh
@@ -1,1 +1,1 @@
-A=0 && for i in ./src/repos/*/*/*/summary.json; do cp ${i} ./src/menu-data/summary_${A}.json && let  A++ ; done
+A=0 && for i in ./src/repos/*/*/summary.json; do cp ${i} ./src/menu-data/summary_${A}.json && let  A++ ; done
