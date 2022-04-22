eb0c7dae66e304622037e7f1d634a5e516eff536
doomling <33739951+doomling@users.noreply.github.com>
2022-04-22 16:13:14 -0300

    algolia front integration

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,12 +2,47 @@ import React from 'react'
 import './types'
 import './style.scss'
 import formatPaths from '@utils/formatPaths'
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits, connectStateResults, Snippet } from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch('ZBR370BA1A', '90d39c58d1ec20ab5f315750f7894b8b');
+
+const Hit = ({ hit }) => {
+  console.log(hit, 123)
+  return (
+    <div className="hit-result-container">
+      <h3>The title</h3>
+      <Snippet hit={hit} attribute="html" />
+    </div>
+  )}
+
+const Results = connectStateResults(({ searchState }) => {
+  return searchState && searchState.query ? <Hits hitComponent={Hit} /> : null 
+})
+
+// // 1. Create a render function
+// const renderSearchBox = ({currentRefinement, refine}) => {
+//   return (
+//     <>
+//       <input placeholder='Search here...'/>
+// {currentRefinement, refine}//     </>
+//   )
+// };
+
+// // 2. Create the custom widget
+// const CustomSearchBox = connectSearchBox(
+//   renderSearchBox
+// );
+
+const glass = <img className='search-bar-icon' src={formatPaths("search.png")} />
 
 export default function Search() {
   return (
-    <div className="search-bar-container">
-      <input placeholder='Search...' />
-      <img className='search-bar-icon' src={formatPaths("search.png")} />
-    </div>
-  )
-}
+      <InstantSearch searchClient={searchClient} indexName="DCL_DOCS">
+        <div className="search-bar-container">
+          <SearchBox submit={glass}/>
+        </div>
+        <Results />
+        {/* <Hits hitComponent={Hit} /> */}
+      </InstantSearch>
+  )}
--- src/html.js
+++ src/html.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import 'decentraland-ui/dist/themes/base-theme.css'
 import "@fontsource/inter"
 import "./styles/global-styles.scss"
 
