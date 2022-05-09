34daec5f71dee558d727508ef33befc659e9cfc5
doomling <33739951+doomling@users.noreply.github.com>
2022-05-09 18:01:45 -0300

    custom 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react'
 import GeneralLayout from '../components/GeneralLayout'
+import { navigate } from "gatsby"
 
 const NotFound = () => {
+
+  React.useEffect(() => {
+    setTimeout(() => navigate('/'), 5000)
+  }, [])
+
    return (
     <GeneralLayout>
-       <div>Not found
-          The site you requested was not found.
-          You will be redirected to the general index in 5 seconds. Or click here</div>
+       <div>
+         <h1>Not found</h1>
+         <p>The site you requested was not found.</p>
+         <p>You will be redirected to the general index in 5 seconds. <a href='/'>Or click here</a></p>
+      </div>
     </GeneralLayout>
   )
 }
