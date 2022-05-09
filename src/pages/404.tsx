a7d42f041d9f1f61c4f496ce4a418490082ecadf
doomling <33739951+doomling@users.noreply.github.com>
2022-05-09 16:47:59 -0300

    style clean up

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
+import GeneralLayout from '../components/GeneralLayout'
 
 const NotFound = () => {
-  return <main>404</main>
+   return (
+    <GeneralLayout>
+       <div>Not found
+          The site you requested was not found.
+          You will be redirected to the general index in 5 seconds. Or click here</div>
+    </GeneralLayout>
+  )
 }
 
 export default NotFound
