168777f02e58e4b4de84396ce62984f4735795a4
gonpombo8 <gonzalo@decentraland.org>
2022-05-11 14:16:16 -0300

    wip

--- src/utils/formatPaths.ts
+++ src/utils/formatPaths.ts
@@ -2,7 +2,7 @@ export default function formatPaths(url: string) {
   console.log({ envs: process.env }, process.env.GATSBY_ENV, process.env.GATSBY_PUBLIC_URL)
   const normalizedUrl = url.toLowerCase()
   const finalPath =
-    process.env.GATSBY_ENV === 'prod'
+    (process.env.GATSBY_ENV === 'prod' && false)
       ? `${process.env.GATSBY_PUBLIC_URL}/${normalizedUrl}`
       : `/${normalizedUrl}`
   return finalPath
