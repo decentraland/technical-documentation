458ce8016e85bafaf1fe0a4cc0b02dc1f7d5df7f
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-05-22 19:45:21 -0300

    Debug

--- src/scripts/hashGenerator.js
+++ src/scripts/hashGenerator.js
@@ -15,7 +15,7 @@ packageJson.homepage = ENV_CONTENT['GATSBY_PUBLIC_URL']
 
 if (packageJson.homepage) {
   const url = new URL(packageJson.homepage)
-  // ENV_CONTENT['GATSBY_PUBLIC_PATH'] = url.pathname
+  ENV_CONTENT['GATSBY_PUBLIC_PATH'] = url.pathname
   ENV_CONTENT['GATSBY_URL'] = url.origin
   // github action outputs. Do not touch.
   console.log('::set-output name=public_url::' + packageJson.homepage)
@@ -37,7 +37,9 @@ console.log(packageJson, 123)
 
 function getPublicUrls() {
   if (!process.env.GEN_STATIC_LOCAL) {
+    console.log("first")
     if (process.env.GITHUB_BASE_REF) {
+      console.log('second')
       // Pull request
       return {
         GATSBY_PUBLIC_URL: `https://sdk-team-cdn.decentraland.org/${packageJson.name}/branch/${process.env.GITHUB_HEAD_REF}`,
