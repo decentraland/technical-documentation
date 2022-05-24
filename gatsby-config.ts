13a63ea06e8f3fad21c9da23223bfdfa60ced8cf
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-05-24 17:02:10 -0300

    algolia local test (#50)
    * algolia local test
    
    * strange build error
    
    * optional options

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -75,9 +75,8 @@ const opts = {
             },
           },
         ],
-        plugins: [`gatsby-remark-images`],
         extensions: [`.md`, `.mdx`]
-      },
+      }
     },
     {
       resolve: `gatsby-plugin-compile-es6-packages`,
@@ -89,30 +88,32 @@ const opts = {
         ],
       },
     },
-    {
-      // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.ALGOLIA_APP_ID,
-        // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
-        // Tip: use Search API key with GATSBY_ prefix to access the service from within components
-        apiKey: process.env.ALGOLIA_API_KEY,
-        indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
-        queries,
-        chunkSize: 10000, // default: 1000
-        settings: {
-          // optional, any index settings
-          // Note: by supplying settings, you will overwrite all existing settings on the index
-        },
-        enablePartialUpdates: true, // default: false
-        matchFields: ['slug', 'modified'], // Array<String> default: ['modified']
-        concurrentQueries: false, // default: true
-        // skipIndexing: true, // default: false, useful for e.g. preview deploys or local development
-        continueOnFailure: false, // default: false, don't fail the build if algolia indexing fails
-      },
-    },
   ],
 };
+
+const algoliaOpts = {
+  // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
+  resolve: `gatsby-plugin-algolia`,
+  options: {
+    appId: process.env.ALGOLIA_APP_ID,
+    // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
+    // Tip: use Search API key with GATSBY_ prefix to access the service from within components
+    apiKey: process.env.ALGOLIA_API_KEY,
+    indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
+    queries,
+    chunkSize: 10000, // default: 1000
+    settings: {
+      // optional, any index settings
+      // Note: by supplying settings, you will overwrite all existing settings on the index
+    },
+    enablePartialUpdates: true, // default: false
+    matchFields: ['slug', 'modified'], // Array<String> default: ['modified']
+    concurrentQueries: false, // default: true
+    // skipIndexing: true, // default: false, useful for e.g. preview deploys or local development
+    continueOnFailure: false, // default: false, don't fail the build if algolia indexing fails
+    modules: []
+  }
+}
 
 if (!process.env.ASSET_PREFIX) {
   delete opts['assetPrefix']
@@ -120,6 +121,10 @@ if (!process.env.ASSET_PREFIX) {
 
 if (!process.env.GATSBY_PUBLIC_PATH) {
   delete opts['pathPrefix']
+}
+
+if (process.env.GITHUB_ACTIONS) {
+  opts.plugins.push(algoliaOpts)
 }
 
 module.exports = opts
