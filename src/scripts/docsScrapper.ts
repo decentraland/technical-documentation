1a9228f738acff3eeb00e015f22ec28c3cef891f
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-06-10 20:23:56 -0300

    sha download test

--- src/scripts/docsScrapper.ts
+++ src/scripts/docsScrapper.ts
@@ -1,9 +1,11 @@
 import { cloneRepos } from './../utils/cloneRepos.js'
 import { cleanUpDependencies } from './../utils/shellCommands.js'
 import { yamlToJson } from './../utils/yamlToJson.js'
+import { downloadRepos } from './../utils/downloadRepos.js'
 
 async function main() {
-  await cloneRepos()
+//  await cloneRepos()
+await downloadRepos()
   await cleanUpDependencies(process.cwd(), './src/repos')
   await yamlToJson("./src/repos/legacy/_data/menu.yml", "./src/repos/legacy/_data/menu.json")
 }
--- src/utils/downloadRepos.ts
+++ src/utils/downloadRepos.ts
@@ -3,14 +3,16 @@ import { rmdirSync } from 'fs'
 import { move, readdir, remove, writeFile } from 'fs-extra'
 import path from 'path'
 import fetch from 'node-fetch'
+import PQueue from 'p-queue'
+import repositoryListJson from './../repositories.json'
 
 export const downloadFile = async function (url: string, dest: string) {
   const data = await (await fetch(url)).arrayBuffer()
   await writeFile(dest, Buffer.from(data))
 }
 
-export const downloadRepoZip = async function (url: string, dest: string) {
-  const zipFilePath = path.resolve(dest, 'temp-zip-project.zip')
+export const downloadRepoZip = async function (url: string, dest: string, index: number) {
+  const zipFilePath = path.resolve(dest, `temp-zip-project${index}.zip`)
   await downloadFile(url, zipFilePath)
 
   const oldFiles = await readdir(dest)
@@ -45,3 +47,14 @@ export const downloadRepoZip = async function (url: string, dest: string) {
   rmdirSync(extractedPath)
   await remove(zipFilePath)
 }
+
+export async function downloadRepos() {
+    const queue = new PQueue({ concurrency: 10 })
+    const currentWorkingDir = process.cwd()
+    const promises = repositoryListJson.repositories.map((repo, i) =>
+      queue.add(async () => {
+        downloadRepoZip(repo.zipUrl, currentWorkingDir + '/src/repos/', i) 
+      })
+    )
+    await Promise.all(promises)
+  }
