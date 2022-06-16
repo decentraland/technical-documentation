ed50765d1b69250dc3f26ec904a377d2ea48fde9
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-06-16 00:20:43 -0300

    missing slash

--- src/components/CustomImg/index.tsx
+++ src/components/CustomImg/index.tsx
@@ -3,7 +3,7 @@ import formatPaths from 'utils/formatPaths'
 
 export default function CustomImg (props : any) {
   
-  const formattedSrc = props.src && formatPaths(props.src.replace('{{ site.baseurl }}', ''))
+  const formattedSrc = props.src && formatPaths(props.src.replace('{{ site.baseurl }}/', ''))
   return (
     <img src={formattedSrc} style={props.style} srcSet={props.srcSet} className={props.className} />
   )
