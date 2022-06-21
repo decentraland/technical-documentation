24330b4d6e69454288fa365517b7c0a1f10c865a
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-06-21 20:04:15 -0300

    Debug/downloading big files  (#62)
    * feat: add unity renderer docs
    
    * add ipurl
    
    * testing
    
    * revert and cleanup
    
    * trim weird characters in json
    
    Co-authored-by: Suduck <adrian@suduck.com>

--- src/scripts/menuSplitter.ts
+++ src/scripts/menuSplitter.ts
@@ -8,11 +8,16 @@ export default function menuSplitter() {
     else {
       files.forEach((file) => {
         try {
-          const data = file != 'legacy.json' && JSON.parse(
-            fs.readFileSync('./src/menu-data/' + file, 'utf8')
-          )
+          const data =
+            file !== 'legacy.json' &&
+            JSON.parse(
+              fs
+                .readFileSync('./src/menu-data/' + file, 'utf8')
+                .toString()
+                .trim()
+            )
 
-          Object.keys(data).map((key) => {         
+          Object.keys(data).map((key) => {
             data[key].map((item) => {
               if (menu[key]) {
                 const categoryIndex = menu[key].findIndex((element) => {
@@ -39,7 +44,7 @@ export default function menuSplitter() {
 
           fs.writeFileSync('./src/repos/menu.json', JSON.stringify(menu))
         } catch (err) {
-          console.error(err)
+          console.error(err, file)
         }
       })
     }
@@ -53,39 +58,41 @@ function mapLegacyMenu() {
       fs.readFileSync('./src/menu-data/legacy.json', 'utf8')
     )
 
-    return data.map(item => {
+    return data.map((item) => {
       return generateMappedObject(item)
     })
-
-  } catch(e) {
+  } catch (e) {
     console.log(e)
   }
 }
 
 function formatName(slug) {
   const splitSlug = slug.split('/')
-  const name = splitSlug[splitSlug.length -1].replace(/([0-9]+(-[0-9]+)+)-/, '').replaceAll('-', ' ').replace('.md', '')
+  const name = splitSlug[splitSlug.length - 1]
+    .replace(/([0-9]+(-[0-9]+)+)-/, '')
+    .replaceAll('-', ' ')
+    .replace('.md', '')
   return name
 }
 
 function generateMappedObject(item) {
   let mappedItem = {}
-    if (item.title) {
-      mappedItem = {...mappedItem, name: item.title}
-    } else {
-      mappedItem = {...mappedItem, name: formatName(item.post)}
-    }
+  if (item.title) {
+    mappedItem = { ...mappedItem, name: item.title }
+  } else {
+    mappedItem = { ...mappedItem, name: formatName(item.post) }
+  }
 
-    if (item.post) {
-      mappedItem = {...mappedItem, slug: '/' + item.post.replace('.md', '')}
-    }
-    if (item.children) {
-      const children = item.children.map(child => {
-        return generateMappedObject(child)
-      })
+  if (item.post) {
+    mappedItem = { ...mappedItem, slug: '/' + item.post.replace('.md', '') }
+  }
+  if (item.children) {
+    const children = item.children.map((child) => {
+      return generateMappedObject(child)
+    })
 
-      mappedItem = {...mappedItem, children }
-    }
+    mappedItem = { ...mappedItem, children }
+  }
   return mappedItem
 }
 
