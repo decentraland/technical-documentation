20f63e8e9c5a35ddc96fc7e618aae99d73309570
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-07-08 18:38:18 -0300

    Feat/creators content (#69)
    * add frontmatter instructions
    
    * creators home page content
    
    * removing unused pages
    
    * missing images
    
    * player content
    
    * typo

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -76,7 +76,7 @@ exports.createPages = async ({ graphql, actions }: any) => {
   })
 
   legacyData.data.allMdx.edges.forEach(({ node }: any) => {
-    const filePath = '/player/player-documentation-main/_posts/'
+    const filePath = 'repos/player/player-documentation-main/_posts/'
     const match = /[0-9]{4}-[0-9]{2}-[0-9]{2}-/i
 
     actions.createPage({
