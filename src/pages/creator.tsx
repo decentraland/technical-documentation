2a8f36fa49606711a82281e0b7e24319cdcade10
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-07-08 14:36:55 -0300

    creators home page content

--- src/pages/creator.tsx
+++ src/pages/creator.tsx
@@ -1,13 +1,32 @@
-import * as React from 'react'
-import 'decentraland-ui/dist/themes/base-theme.css'
-import 'decentraland-ui/dist/themes/alternative/light-theme.css'
-import Welcome from '../components/Welcome'
+import React from 'react'
 import SidebarLayout from '../components/SidebarLayout'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
-export default function IndexPage(props) {
+export default function CreatorPage({ data }: any) {
+  const { mdx } = data
+  const { body } = mdx
+
   return (
     <SidebarLayout>
-      <Welcome />
+      <MDXRenderer>{body}</MDXRenderer>
     </SidebarLayout>
   )
 }
+
+export const query = graphql`
+  query ($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      fields {
+        slug
+      }
+      body
+      tableOfContents
+      fileAbsolutePath
+      frontmatter {
+        slug
+        title
+      }
+    }
+  }
+`
