ad8deab6405ef9415c271e0c2605c2dbddb18566
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-07-11 19:39:21 -0300

    contributor home

--- src/components/CustomImg/index.tsx
+++ src/components/CustomImg/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import formatPaths from 'utils/formatPaths'
 
 export default function CustomImg(props: any) {
+  console.log(props.src, 1234)
   const formattedSrc = props.src && formatPaths(props.src.replace('{{ site.baseurl }}/', ''))
   return <img src={formattedSrc} style={props.style} srcSet={props.srcSet} className={props.className} />
 }
