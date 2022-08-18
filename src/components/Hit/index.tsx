c28e4cc84f8bf099e4b1b06cfa5f1bee37e10e1b
doomling <33739951+doomling@users.noreply.github.com>
2022-08-18 18:53:29 -0300

    responsive menu updates

--- src/components/Hit/index.tsx
+++ src/components/Hit/index.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react'
 import { Snippet } from 'react-instantsearch-dom'
 import { Link } from 'gatsby'
-import "./style.scss"
+import './style.scss'
 import formatPaths from '../../utils/formatPaths'
 
 export default function Hit({ hit }) {
-    const imgIndex = hit.frontmatter.slug.split('/')
-    const img = imgIndex[0] === '/' ? imgIndex[2] : imgIndex[1]
-    return (
-      <div className="hit-result">
-        <div className='hit-result-icon'>
-          <img src={formatPaths(`${img}.svg`)} />
-        </div>
-        <Link to={hit.frontmatter.slug} >
-          <h4 className='hit-result-title'>{hit.frontmatter.title}</h4>
-          <Snippet attribute="html" hit={hit} />
-        </Link>
+  const imgIndex = hit.frontmatter.slug.split('/')
+  const img = imgIndex[0] === '/' ? imgIndex[2] : imgIndex[1]
+  return (
+    <div className="hit-result">
+      <div className="hit-result-icon">
+        <img src={formatPaths(`${img}.svg`)} />
       </div>
-    )}
+      <Link to={hit.frontmatter.slug}>
+        <h4 className="hit-result-title">{hit.frontmatter.title}</h4>
+        <Snippet attribute="html" hit={hit} />
+      </Link>
+    </div>
+  )
+}
--- src/components/ResponsiveSidebar/index.tsx
+++ src/components/ResponsiveSidebar/index.tsx
@@ -1,42 +1,71 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import RootDir from '../RootDir'
 import './style.scss'
 import menu from '../../repos/menu.json'
 import Search from '../Search'
 import formatPaths from 'utils/formatPaths'
+import { SidebarProvider } from '../../contexts/Sidebar'
 
 export default function ResponsiveSidebar({ category, properties }: any) {
   const [open, setOpen] = useState<boolean>(false)
+  const [activeValue, setActiveValue] = useState<string>('Menu')
   const match = /[0-9]{4}-[0-9]{2}-[0-9]{2}-/i
+  const [showSearch, setShowSearch] = useState<boolean>(false)
+  const { bgColor } = properties
 
+  function updateValue(newValue: string) {
+    setActiveValue(newValue)
+  }
+
+  console.log(properties)
 
   return (
-    <div className={'responsive-sidebar-container'} style={{ borderBottom: `3px solid ${properties.bgColor}` }}>
-      {category && properties && (
-        <>
-          <div className="responsive-sidebar-header" onClick={() => setOpen((prevState) => !prevState)}>
-            {/* <Search /> */}
-            <h2 className="responsive-sidebar-title">
-              {category} Documentation{' '}
-              <img src={formatPaths('caret-black.png')} style={open ? { transform: 'rotate(90deg)' } : {}} />
-            </h2>
-          </div>
-          <div className={open ? `responsive-sidebar-items` : `responsive-sidebar-items-closed`}>
-            {menu &&
-              menu[category].map((item: any, key: number) => {
-                return (
-                  <RootDir
-                    name={item.name}
-                    offset={0}
-                    children={item.children}
-                    slug={item.slug?.toLowerCase().replace(match, '')}
-                    key={key}
-                  />
-                )
-              })}
-          </div>
-        </>
-      )}
-    </div>
+    <SidebarProvider value={{ activeMenu: 'Menu', updateValue }}>
+      <div className={'responsive-sidebar-container'}>
+        {category && properties && (
+          <>
+            {showSearch ? (
+              <div className="responsive-sidebar-searchbox">
+                <div className='search-wrapper'><Search /></div>
+                <span onClick={() => setShowSearch((prevState) => !prevState)} style={{ color: bgColor }}>
+                  cancel
+                </span>
+              </div>
+            ) : (
+              <div className="responsive-sidebar-header">
+                <h2
+                  className="responsive-sidebar-title"
+                  onClick={() => setOpen((prevState) => !prevState)}
+                  style={{ color: `${properties.bgColor}` }}
+                >
+                  {activeValue}
+                  <img src={formatPaths('responsive-caret.png')} />
+                </h2>
+                <img
+                  className="search-bar-icon"
+                  src={formatPaths('search.png')}
+                  onClick={() => setShowSearch((prevState) => !prevState)}
+                />
+              </div>
+            )}
+            <div className={open ? `responsive-sidebar-items` : `responsive-sidebar-items-closed`}>
+              <img className="icon-close" src={formatPaths('close.svg')} onClick={() => setOpen(false)} />
+              {menu &&
+                menu[category].map((item: any, key: number) => {
+                  return (
+                    <RootDir
+                      name={item.name}
+                      offset={0}
+                      children={item.children}
+                      slug={item.slug?.toLowerCase().replace(match, '')}
+                      key={key}
+                    />
+                  )
+                })}
+            </div>
+          </>
+        )}
+      </div>
+    </SidebarProvider>
   )
 }
