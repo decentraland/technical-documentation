116eb5b836233a414ca13f3885adac01191dd972
doomling <33739951+doomling@users.noreply.github.com>
2022-08-24 18:44:11 -0300

    testing internal links prodlike

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -3,6 +3,8 @@ import './style.scss'
 
 export default function CustomLink(props: any) {
   const { href, children, id } = props
+
+  console.log(props, 123333)
   return (
     <a className="blog-link" href={href} id={id && id}>
       {children}
