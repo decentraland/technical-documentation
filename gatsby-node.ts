ed0a12a930ec4ef72ebbcb92846f562da32597f9
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-08-24 13:24:05 -0300

    Fix/cleanup 22 08 (#88)
    * doc links and height
    
    * player redirects
    
    * wrong paths
    
    * removed slash
    
    * redirects for non player pages
    
    * removing optional field from query

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -55,7 +55,6 @@ exports.createPages = async ({ graphql, actions }: any) => {
               slug
             }
             frontmatter {
-              redirect_from
               title
             }
           }
@@ -72,6 +71,16 @@ exports.createPages = async ({ graphql, actions }: any) => {
         slug: node.fields.slug.toLowerCase()
       }
     })
+
+    if (node.frontmatter.redirect_from) {
+      node.frontmatter.redirect_from.map((item) => {
+        actions.createRedirect({
+          fromPath: item,
+          toPath: node.fields.slug.toLowerCase(),
+          isPermanent: true
+        })
+      })
+    }
   })
 
   legacyData.data.allMdx.edges.forEach(({ node }: any) => {
