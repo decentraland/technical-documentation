879dc893b028b2ccfcd114461d162de6380f0c1e
doomling <33739951+doomling@users.noreply.github.com>
2022-08-25 18:06:33 -0300

    cosa

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import './style.scss'
-
-function appendDomain(url) {
-  if (url.includes(process.env.GATSBY_PUBLIC_PATH)) {
-    return url.replace(process.env.GATSBY_PUBLIC_PATH, '')
-  } else {
-    return url
-  }
-}
+import { useStaticQuery, graphql } from 'gatsby'
 
 export default function CustomLink(props: any) {
   const { href, children, id } = props
 
-  console.log(process.env.GATSBY_PUBLIC_PATH, 'the path', process.env)
+  const { site } = useStaticQuery(graphql`
+    {
+      site {
+        assetPrefix
+      }
+    }
+  `)
+
+  function appendDomain(url) {
+    if (url.includes(site.assetPrefix)) {
+      return url.replace(site.assetPrefix, '')
+    } else {
+      return url
+    }
+  }
+
+  console.log(site)
 
   return (
     <a className="blog-link" href={href && appendDomain(href)} id={id && id}>
