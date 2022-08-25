924313a5515b9a1c587a2325aa461422978764e2
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-08-25 02:46:17 -0300

    test links (#89)
    * test links
    
    * test fix
    
    * test fix

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -4,10 +4,17 @@ import './style.scss'
 export default function CustomLink(props: any) {
   const { href, children, id } = props
 
-  console.log(props, 123333
-    )
+  function appendDomain(url) {
+    if (url.includes(process.env.GATSBY_PUBLIC_PATH)) {
+      return url.replace(process.env.GATSBY_PUBLIC_PATH, '')
+    } else {
+      return url
+    }
+  }
+
+  console.log(props, 'curtom link props')
   return (
-    <a className="blog-link" href={href} id={id && id}>
+    <a className="blog-link" href={href && appendDomain(href)} id={id && id}>
       {children}
     </a>
   )
