2e426ab1c78b7c5a927ab8647d19a09b840c18a0
doomling <33739951+doomling@users.noreply.github.com>
2022-08-25 18:32:01 -0300

    remove assetPrefix on intenal links

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -2,14 +2,6 @@ import React from 'react'
 import './style.scss'
 import { useStaticQuery, graphql } from 'gatsby'
 
-function appendDomain(url) {
-  if (url.includes(process.env.GATSBY_PUBLIC_PATH)) {
-    return url.replace(process.env.GATSBY_PUBLIC_PATH, '')
-  } else {
-    return url
-  }
-}
-
 export default function CustomLink(props: any) {
   const { href, children, id } = props
 
@@ -21,18 +13,14 @@ export default function CustomLink(props: any) {
     }
   `)
 
-  function appendDomain(url) {
-    if (url.includes(site.assetPrefix)) {
-      return url.replace(site.assetPrefix, '')
-    } else {
-      return url
-    }
+  let newHref = href
+  const assetPrefix = site.assetPrefix.replace('https://', 'https:/')
+  if (href.startsWith(assetPrefix)) {
+    newHref = href.slice(assetPrefix.length)
   }
 
-  console.log(site)
-
   return (
-    <a className="blog-link" href={href && appendDomain(href)} id={id && id}>
+    <a className="blog-link" href={newHref} id={id && id}>
       {children}
     </a>
   )
