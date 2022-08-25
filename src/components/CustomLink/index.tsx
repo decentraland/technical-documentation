13554341b6461ce9b03f3505a76335f14404dae4
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-08-25 17:38:44 -0300

    Test ci (#92)
    * remove logs
    
    * logs
    
    * :broom:
    
    * nope
    
    * undo
    
    * test
    
    * logs
    
    * test

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './style.scss'
 
+function appendDomain(url) {
+  if (url.includes(process.env.GATSBY_PUBLIC_PATH)) {
+    return url.replace(process.env.GATSBY_PUBLIC_PATH, '')
+  } else {
+    return url
+  }
+}
+
 export default function CustomLink(props: any) {
   const { href, children, id } = props
 
-  function appendDomain(url) {
-    if (url.includes(process.env.GATSBY_PUBLIC_PATH)) {
-      return url.replace(process.env.GATSBY_PUBLIC_PATH, '')
-    } else {
-      return url
-    }
-  }
+  console.log(process.env.GATSBY_PUBLIC_PATH, 'the path', process.env)
 
-  console.log(process.env.GATSBY_PUBLIC_PATH, 'the path')
-  
   return (
     <a className="blog-link" href={href && appendDomain(href)} id={id && id}>
       {children}
--- src/scripts/hashGenerator.js
+++ src/scripts/hashGenerator.js
@@ -28,8 +28,17 @@ if (packageJson.homepage) {
   console.log('::set-output name=public_path::' + url.pathname)
 }
 
+console.log(ENV_CONTENT)
+
 fs.writeFileSync(
   `.env`,
+  Object.entries(ENV_CONTENT)
+    .map((e) => e[0] + '=' + JSON.stringify(e[1]))
+    .join('\n') + '\n'
+)
+
+fs.writeFileSync(
+  `.env.production`,
   Object.entries(ENV_CONTENT)
     .map((e) => e[0] + '=' + JSON.stringify(e[1]))
     .join('\n') + '\n'
