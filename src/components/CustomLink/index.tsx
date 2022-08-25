45a86344b87604a527d27f13e2f3dd7a0fd7cc81
doomling <33739951+doomling@users.noreply.github.com>
2022-08-25 13:42:11 -0300

    trigger pipeline

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -13,6 +13,7 @@ export default function CustomLink(props: any) {
   }
 
   console.log(props, 'curtom link props')
+  
   return (
     <a className="blog-link" href={href && appendDomain(href)} id={id && id}>
       {children}
