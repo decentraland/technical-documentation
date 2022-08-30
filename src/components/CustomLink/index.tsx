9b9da96e40ed66ce9f4a599d6e995db31e5dd470
doomling <33739951+doomling@users.noreply.github.com>
2022-08-30 19:49:47 -0300

    algolia index and optional query

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -2,28 +2,31 @@ import React from 'react'
 import './style.scss'
 import { useStaticQuery, graphql } from 'gatsby'
 
+function removeAppend(url) {
+  let newHref = url
+
+  if (!process.env.GATSBY_PUBLIC_PATH) {
+    const { site } = useStaticQuery(graphql`
+      {
+        site {
+          assetPrefix
+        }
+      }
+    `)
+
+    const assetPrefix = site.assetPrefix.replace('https://', 'https:/')
+    if (url.startsWith(assetPrefix)) {
+      newHref = url.slice(assetPrefix.length)
+    }
+  }
+
+  return newHref
+}
+
 export default function CustomLink(props: any) {
   const { href, children, id } = props
 
   // this is a temporal workaround, see https://github.com/gatsbyjs/gatsby/issues/21462
-
-  const { site } = useStaticQuery(graphql`
-    {
-      site {
-        assetPrefix
-      }
-    }
-  `)
-
-  function removeAppend(url) {
-    let newHref = url
-    const assetPrefix = site.assetPrefix.replace('https://', 'https:/')
-    if (href.startsWith(assetPrefix)) {
-      newHref = href.slice(assetPrefix.length)
-    }
-
-    return newHref
-  }
 
   return (
     <a className="blog-link" href={href && removeAppend(href)} id={id && id}>
