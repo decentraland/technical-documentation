b2c844e92a92d7d4f4e60aebac7ef6373a00e03b
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-09-08 17:35:43 -0300

    cleanup

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import formatPaths from './src/utils/formatPaths'
 
-const HeadComponents = [<><script key="rollbar" src={formatPaths("/scripts/rollbar.js")} /><script key="segment" src={formatPaths("/scripts/segment.js")} /></>];
+const HeadComponents = [<><script key="rollbar" src={formatPaths("scripts/rollbar.js")} /><script key="segment" src={formatPaths("scripts/segment.js")} /></>];
 
 const onRenderBody = ({ setHeadComponents }) => {
   // if (process.env.CI) {
