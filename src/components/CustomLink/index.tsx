bbe109279c4598049f1d591d374ea415090d04ee
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-09-08 13:53:46 -0300

    Fix/all paths (#112)
    * paths
    
    * minor fixes
    
    * code margins
    
    * check for undefined content
    
    * fix internal links and logs
    
    * test images
    
    * remove verbose

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -1,44 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.scss'
-
-function removeAppend(url, location) {
-  let newHref = url
-
-  // this is a temporal workaround, see https://github.com/gatsbyjs/gatsby/issues/21462
-  const assetPrefix = process.env.GATSBY_ASSET_PREFIX?.replace('https://', 'https:/')
-
-  if (url.startsWith(assetPrefix)) {
-    console.log('entra')
-    newHref = url.slice(assetPrefix.length)
-  }
-
-  // sanitize url for anchors if someone enters an ending slash
-
-  const { origin, pathname } = location
-  if (url.startsWith('#')) {
-    if (pathname.endsWith('/')) {
-      newHref = `${origin}${pathname.slice(0, -1)}${url}`
-    }
-  }
-
-  return newHref
-}
 
 export default function CustomLink(props: any) {
   const { href, children, id, location } = props
-  const isAnchor = href && href.startsWith('#')
-  
+  // const isAnchor = href && href.startsWith('#')
+
+  const [sanitizedUrl, setSanitizedUrl] = useState<string>()
+  const [isInternal, setIsInternal] = useState(false)
+
+  function removeAppend(url, location) {
+    let newHref = url
+
+    // this is a temporal workaround, see https://github.com/gatsbyjs/gatsby/issues/21462
+    const assetPrefix = process.env.GATSBY_ASSET_PREFIX?.replace('https://', 'https:/')
+
+    if (url.startsWith(assetPrefix)) {
+      newHref = url.slice(assetPrefix.length)
+    }
+
+    // sanitize url for anchors if someone enters an ending slash
+
+    const { origin, pathname } = location
+    if (url.startsWith('#')) {
+      if (pathname.endsWith('/')) {
+        newHref = `${origin}${pathname.slice(0, -1)}${url}`
+      }
+    }
+
+    setSanitizedUrl(newHref)
+  }
+
+  useEffect(() => {
+    if (href) {
+      removeAppend(href, location)
+      if (sanitizedUrl) {
+        if (sanitizedUrl.startsWith('/') || sanitizedUrl.startsWith('#')) {
+          setIsInternal(true)
+        }
+      }
+    }
+  }, [sanitizedUrl])
 
   return (
     // Conditional rendering to account for anchors with no href
     <>
       {href ? (
-        <a
-          className="blog-link"
-          href={removeAppend(href, location)}
-          id={id && id}
-          target={isAnchor ? '_self' : '_blank'}
-        >
+        <a className="blog-link" href={sanitizedUrl} id={id && id} target={isInternal ? '_self' : '_blank'}>
           {children}
         </a>
       ) : (
