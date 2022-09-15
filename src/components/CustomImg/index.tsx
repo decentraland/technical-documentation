91120033fe0fc4c43d167644b079773f8f96c682
Bel Rey <33739951+doomling@users.noreply.github.com>
2022-09-15 20:45:13 -0300

    Chore/allow external images (#127)
    * allow external image srcs
    
    * temporal creators branch
    
    * Update src/components/CustomImg/index.tsx
    
    * Update src/components/CustomImg/index.tsx
    
    Co-authored-by: Gon Pombo <gonzalo@decentraland.org>

--- src/components/CustomImg/index.tsx
+++ src/components/CustomImg/index.tsx
@@ -3,6 +3,11 @@ import formatPaths from 'utils/formatPaths'
 import './style.scss'
 
 export default function CustomImg(props: any) {
-  const formattedSrc = props.src && formatPaths(props.src.replace('{{ site.baseurl }}/', '').replace('/', ''))
+  let formattedSrc = props.src
+
+  if (formattedSrc && !formattedSrc.startsWith('https://')) {
+    formattedSrc = props.src && formatPaths(props.src.replace('{{ site.baseurl }}/', '').replace('/', ''))
+  }
+
   return <img src={formattedSrc} style={props.style} srcSet={props.srcSet} className={`${props.className} md-images`} />
 }
