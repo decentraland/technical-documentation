0c204cf78f52b78aea5a3c869683aebcda55f46b
gonpombo8 <gonzalo@decentraland.org>
2022-09-19 13:46:52 -0300

    add custom snippet

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -161,17 +161,17 @@ const opts = {
         // optional; non-empty string
         devKey: process.env.GATSBY_SEGMENT_API_KEY_DEV,
 
-        trackPage: true,
+        // trackPage: true,
 
         // number (defaults to 50); time to wait after a route update before it should
         // track the page change, to implement this, make sure your `trackPage` property is set to `true`
-        trackPageDelay: 50,
+        // trackPageDelay: 50,
 
         delayLoad: false,
         manualLoad: false,
 
 
-        // customSnippet: '!function(){var analytics=window.analytics||[];...;analytics.load("${writeKey}");analytics.page();}}();'
+        customSnippet: '!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${writeKey}";;analytics.SNIPPET_VERSION="4.15.3";analytics.load("${writeKey}");analytics.page();}}();'
       },
     },
     'gatsby-plugin-meta-redirect' // make sure this is always the last one
